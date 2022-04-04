365633ee2b135639d6cc6b7a39f6d63863e9b93c
Your Namegit config --global user.name Your <you@example.com>
2022-04-04 14:03:58 -0400

    Final Draft

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-const inventory = newInventory()
-move(inventory).to(0, 0)
+const inventory = newInventory();
+move(inventory).to(0, 0);
 
-const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+const character = newImage("assets/green-character/static.gif");
 
+function changeDirection(direction) {
+  if (direction === null) {
+    character.src = `assets/green-character/static.gif`;
+  }
+  if (direction === "west") {
+    character.src = `assets/green-character/west.gif`;
+  }
+  if (direction === "north") {
+    character.src = `assets/green-character/north.gif`;
+  }
+  if (direction === "east") {
+    character.src = `assets/green-character/east.gif`;
+  }
+  if (direction === "south") {
+    character.src = `assets/green-character/south.gif`;
+  }
+}
 
-move(newImage('assets/tree.png')).to(200, 450)
-move(newImage('assets/pillar.png')).to(350, 250)
-move(newImage('assets/pine-tree.png')).to(450, 350)
-move(newImage('assets/crate.png')).to(150, 350)
-move(newImage('assets/well.png')).to(500, 575)
-move(newItem('assets/sword.png')).to(500, 555)
-move(newItem('assets/shield.png')).to(165, 335)
-move(newItem('assets/staff.png')).to(600, 250)
+move(character).withArrowKeys(100, 250, changeDirection);
+
+move(newImage("assets/tree.png")).to(200, 450);
+move(newImage("assets/pillar.png")).to(350, 250);
+move(newImage("assets/pine-tree.png")).to(450, 350);
+move(newImage("assets/crate.png")).to(150, 350);
+move(newImage("assets/well.png")).to(500, 575);
+move(newItem("assets/sword.png")).to(500, 555);
+move(newItem("assets/shield.png")).to(165, 335);
+move(newItem("assets/staff.png")).to(600, 250);
